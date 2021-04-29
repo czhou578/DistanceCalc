080de7bfaf1156c8d4fe1f0ea62fc697dc56d5d2
Colin Zhou <colin.cosun@gmail.com>
2021-04-28 17:15:00 -0700

    commit

--- my-app/src/components/ResultCard.jsx
+++ my-app/src/components/ResultCard.jsx
@@ -7,11 +7,20 @@ export default class ResultCard extends Component {
     super(props)
   }
 
+  didDeleteData = () => {
+    console.log(this.props.deleteData)
+    if (this.props.deleteData) {
+      return document.getElementById('distance').innerHTML = 'Distance: '
+    } else {
+      return this.props.didChangeDistance
+    }
+  }
+
   render() {
     return (
       <div className="result">
-        <h2>Distance: {this.props.didChangeDistance}</h2>
-        <h2>Travel time: {this.props.didChangeTime()} </h2>
+        <h2 id="distance">Distance: {this.props.didChangeDistance} {this.didDeleteData}</h2>
+        <h2 id="travelTime">Travel time: {this.props.didChangeTime()} </h2>
       </div>
     )
   }
