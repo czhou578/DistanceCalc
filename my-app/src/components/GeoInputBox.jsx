7d4b022cf811044aa9912cec2466afad7c1ed051
= <colizu2020@gmail.com>
2021-05-11 10:04:10 -0700

    commit

--- my-app/src/components/GeoInputBox.jsx
+++ my-app/src/components/GeoInputBox.jsx
@@ -9,7 +9,46 @@ export default class GeoInputBox extends Component {
     this.firstRef = React.createRef()
     this.secondRef = React.createRef()
 
+    this.state = {
+      data: [
+        {
+          firstCity: "",
+          secondCity: "",
+          firstCityLatitude: "",
+          secondCityLatitude: "",
+          firstCityLongitude: "",
+          secondCityLongitude: "",
+        }
+      ]
+    }
   }
+
+  returnGeoData = () => {
+    const enteredFirstCity = ReactDOM.findDOMNode(this.firstRef.current)
+    const enteredSecondCity = ReactDOM.findDOMNode(this.secondRef.current)
+
+    this.setState({firstCity: enteredFirstCity, secondCity: enteredSecondCity}, 
+      function() {
+        const data = {"location": enteredFirstCity, "options": {"thumbmaps": false}}
+        const data2 = {"location": enteredSecondCity, "options": {"thumbmaps": false}}
+        fetch('http://www.mapquestapi.com/geocoding/v1/address?key=HACo4SAj1MJfWSocfZTAEkOOlHd0xrIB'), {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(data)
+        },
+        fetch('http://www.mapquestapi.com/geocoding/v1/address?key=HACo4SAj1MJfWSocfZTAEkOOlHd0xrIB'), {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(data2)
+        }
+        .then(res => {return res.json()})
+      })
+  }
+
 
   render() {
     return (
