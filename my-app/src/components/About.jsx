7b8725e1886f13c29264a4919d033969546f6577
= <colizu2020@gmail.com>
2021-07-29 11:04:22 -0700

    commit

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import ColinPic from './colin.jpg'
 
 export default function About(props) {
   const styles = {
@@ -9,13 +9,28 @@ export default function About(props) {
 
   const styles2 = {
     marginTop: '50px',
-    marginBottom: '10px'
+    marginBottom: '10px',
+    marginLeft: '30%'
+  }
+
+  const picStyle = {
+    height: '300px',
+    width: '500px',
+    objectFit: 'cover'
   }
 
 
   return (
     <div style={styles2}>
       <h1 style={styles}>About the maker: Colin Zhou</h1>
+      <p style={styles}>Colin Zhou is a Canadian American designer of Chinese
+        descent who loves making responsive web applications. <br></br>He is 
+        excited to present this easy to use calculator for the benefit
+        of the public. 
+      </p>
+      <div className="pic">
+        <img src={ColinPic} alt="" style={picStyle}/>
+      </div>
     </div>
   )
 }
