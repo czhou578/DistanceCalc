5311a97d5da81edac26b1365898299e0b120267a
= <colizu2020@gmail.com>
2021-07-30 22:43:15 -0700

    commit

--- my-app/src/components/SatisfactionForm.jsx
+++ my-app/src/components/SatisfactionForm.jsx
@@ -19,10 +19,10 @@ export default function(props) {
     console.log('hello')
   })
 
-  const validationSchema = Yup.object({
-    firstName: Yup.string().required('Required'),
-    lastName: Yup.string().required('Required'),
-    email: Yup.string().required('Required')
+  const validationSchema = yup.object({
+    firstName: yup.string().required('Required').max(5),
+    lastName: yup.string().required('Required'),
+    email: yup.string().required('Required')
   })
 
   function incrementCount(count) {
@@ -37,30 +37,51 @@ export default function(props) {
       initialValues={initialValues}
       validationSchema={validationSchema}
       enableReinitialize
+      validateOnChange={true}
     >
-      <div>
-        <h1>Want to Subscribe?</h1>
-        <div className={styles['text-wrapper']}>
-          <TextField id="standard-basic" label="First Name" color="red"/>
+      {(formProps) => {
+        const {
+          handleChange,
+          handleSubmit,
+          isValid,
+          isSubmitting,
+          dirty,
+        } = formProps;
+        return (
           <div>
-            <TextField id="standard-basic2" label="Last Name" color="red"/>
-          </div>
-          <div>
-            <TextField id="standard-basic3" label="Email Address" color="red"/>
-          </div>
-          <div className={styles.rating}>
-            Rating (1-10)
-            <button onClick={incrementCount} className={styles.btn}>
-              +
-            </button>
-            <div ref={ref1}>{count}</div>
-          </div>
-          <Button variant="contained" color="primary">
-            Submit
-          </Button>
-        </div>
+            <h1>Want to Subscribe?</h1>
+            <div className={styles['text-wrapper']}>
+              <form action="submit">
+                <TextField id="standard-basic" label="First Name" color="red"
+                  onChange={handleChange}
+                  name="firstName"
 
-      </div>
+                />
+                <div>
+                  <TextField id="standard-basic2" label="Last Name" color="red"/>
+                </div>
+                <div>
+                  <TextField id="standard-basic3" label="Email Address" color="red"/>
+                </div>
+                <div className={styles.rating}>
+                  Rating (1-10)
+                  <button onClick={incrementCount} className={styles.btn}>
+                    +
+                  </button>
+                  <div ref={ref1}>{count}</div>
+                </div>
+                <Button variant="contained" color="primary">
+                  Submit
+                </Button>
+
+              </form>
+            </div>
+
+          </div>
+
+
+        )
+      }}
 
 
     </Formik>
